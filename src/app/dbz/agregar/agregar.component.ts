e19018d26138c39135181a8ed44db70660b0e53f
maqalher <maqalher@hotmail.com>
2022-01-24 00:27:03 -0600

    add services

---
+++ src/app/dbz/agregar/agregar.component.ts
@@ -0,0 +1,48 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-agregar',
+  templateUrl: './agregar.component.html'
+})
+export class AgregarComponent {
+
+  // @Input() personajes: Personaje[] = [];
+  // agregar(event:any) {
+  //   event.preventDefault();
+  //   console.log(event)
+  // }
+
+  @Input()
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  // @Output() onNuevoPersonaje: EventEmitter<Personaje> = new EventEmitter();
+
+  constructor(private dbzService:DbzService) {}
+
+  agregar() {
+    if (this.nuevo.nombre.trim().length === 0) {
+      return
+    }
+    console.log(this.nuevo)
+
+    // this.personajes.push(this.nuevo)
+    // this.onNuevoPersonaje.emit( this.nuevo );
+
+    this.dbzService.agregarPersonaje(this.nuevo);
+
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+  }
+
+  // cambiarNombre(event: any) {
+  //   console.log(event.target.value)
+  // }
+}
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+
 
   ngOnInit(): void {
+  }
+
+  // personajes: Personaje[] = [];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  }
+
+  // agregarNuevoPersonaje(arguemnto:Personaje) {
+  //   // console.log(arguemnto);
+  //   this.personajes.push(arguemnto);
+  // }
+
+    // get personajes():Personaje[] {
+    //   return this.dbzService.personajes;
+    // }
+
+  constructor( private dbzService: DbzService) {
+    // this.personajes = this.dbzService.personajes;
   }
 
 }
